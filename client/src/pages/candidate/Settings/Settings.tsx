22bdc8720f10e72b38dfb9cfb7e58f67f80da2a9
Archi <archigarg329@gmail.com>
2026-07-19 14:59:21 +0530

    refactor: remove notification and privacy settings components; streamline settings API interactions

--- client/src/pages/candidate/Settings/Settings.tsx
+++ client/src/pages/candidate/Settings/Settings.tsx
@@ -1,9 +1,6 @@
 import { SectionTitle } from "../../../components/admin/common";
 import ProfileSettings from "../../../components/candidate/settings/ProfileSettings";
 import PasswordSettings from "../../../components/candidate/settings/PasswordSettings";
-import NotificationSettings from "../../../components/candidate/settings/NotificationSettings";
-import PrivacySettings from "../../../components/candidate/settings/PrivacySettings";
-import SessionManagement from "../../../components/candidate/settings/SessionManagement";
 
 export default function Settings() {
     return (
@@ -15,17 +12,8 @@ export default function Settings() {
 
             <div className="grid gap-6 lg:grid-cols-2">
                 <ProfileSettings />
-
                 <PasswordSettings />
-
-                <NotificationSettings />
-
-                <PrivacySettings />
-
-                <SessionManagement />
-
             </div>
-
         </div>
     );
 }
